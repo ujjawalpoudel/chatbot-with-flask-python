b2e2497622338d2c226bb7ebaea2070643a21e3f
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2023-04-16 22:20:36 -0400

    delete static url of mongoengine

--- config.py
+++ config.py
@@ -8,6 +8,4 @@ database_name = os.getenv("DATABASE_NAME")
 host = os.getenv("DB_HOST")
 
 # * Define the database host URI
-# host_uri=f"mongodb://{user}:{password}@{host}/{database_name}"
-host_uri="mongodb+srv://medadmin:hMna4RYKa0rIS9wp@medicalanalysissystem.mptbxix.mongodb.net/medicalanalysissystem"
-
+host_uri=f"mongodb://{user}:{password}@{host}/{database_name}"
